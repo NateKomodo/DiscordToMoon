da92c7fcd00fa059dfaf0684fbe385c3176528f9
natekomodo <natkomodo@gmail.com>
2021-02-06 20:11:29 +0000

    update build

--- DiscordToMoon/build.sh
+++ DiscordToMoon/build.sh
@@ -1,3 +1,3 @@
-dotnet publish -r ubuntu.16.04-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true --self-contained
+dotnet publish -r ubuntu.20.04-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true --self-contained
 dotnet publish -r win10-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true --self-contained
 
